86d27daa47be9043ae3c021bd84388814ee116b9
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-16 12:07:12 +0300

    chore: install heroui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+import { heroui } from '@heroui/theme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@heroui/theme/dist/components/(button|modal|navbar|table|ripple|spinner|checkbox|form|spacer).js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [heroui()],
 };
