6a0d167fadadb447405b3520c72b8e0c25cb67f5
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-16 12:32:06 +0300

    chore: update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ import { heroui } from '@heroui/theme';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/@heroui/theme/dist/components/(button|modal|navbar|table|ripple|spinner|checkbox|form|spacer).js',
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@heroui/theme/dist/components/(button|input|modal|navbar|table|ripple|spinner|form|checkbox|spacer).js"
   ],
   theme: {
     extend: {},
