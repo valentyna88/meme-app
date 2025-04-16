003f8b1805644134b89200c851dd82d2981fce95
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-04-16 13:20:16 +0300

    feat: add AddMemeModal component

---
+++ src/components/AddMemeModal.jsx
@@ -0,0 +1,42 @@
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from '@heroui/react';
+import { Input } from '@heroui/input';
+import { Button } from '@heroui/button';
+
+const AddMemeModal = ({ isOpen, onClose, newMeme, setNewMeme, onSave }) => {
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onClose}>
+      <ModalContent>
+        <ModalHeader>Create New Meme</ModalHeader>
+        <ModalBody>
+          <Input
+            label="Meme Name"
+            value={newMeme.name}
+            onChange={e => setNewMeme({ ...newMeme, name: e.target.value })}
+          />
+          <Input
+            label="Likes"
+            type="number"
+            value={newMeme.likes}
+            onChange={e => setNewMeme({ ...newMeme, likes: e.target.value })}
+          />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" variant="light" onPress={onClose}>
+            Close
+          </Button>
+          <Button color="primary" onPress={onSave}>
+            Save
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default AddMemeModal;
